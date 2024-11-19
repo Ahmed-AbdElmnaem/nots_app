c58486c07de88562a7b7f6795540b5a3992fa5e7
Ahmed-AbdElmnaem <ahmedabdelmnaem6@gmail.com>
2024-11-19 23:40:46 +0200

    cearte type adabter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,19 @@
-class NoteModel {
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
-  NoteModel({
+  @HiveField(3)
+  final int color;
+  NoteModel(
+    this.color, {
     required this.title,
     required this.subtitle,
     required this.date,
