3260e12a2bdb7f7877bf694dfcf48f701047af57
Ahmed-AbdElmnaem <ahmedabdelmnaem6@gmail.com>
2024-11-19 23:45:45 +0200

    int hive and openbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/Consts.dart';
 import 'package:notes_app/views/nots_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(knoteBox);
+
   runApp(const NotsApp());
 }
 
